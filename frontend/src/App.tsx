9611966f6ff920850b089a6a4cfc0f8eb7bb0904
thards89 <thards89@gmail.com>
2024-03-04 18:19:30 +0000

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <main></main>
+      <footer>All rights reserved</footer>
+    </div>
+  )
+}
+
+export default App
